c0897bb2a838a67c851c10abf9aec9da1f426139
rezatrimahardhika <rezatrimahardhika@gmail.com>
2020-09-08 01:51:35 +0700

    add start3

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const Discord = require('discord.js');
 const bot = new Discord.Client();
 const TOKEN = process.env.TOKEN;
 
-bot.login(TOKEN);
+bot.login("NzUyNTEyNzExNjA2NDAzMDcy.X1YuEw._Ife4furhQFxFNAOTaTqekvqKbQ");
 
 function random_item(items) {
   return items[Math.floor(Math.random()*items.length)];
