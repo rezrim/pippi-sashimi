639e40e51b9f9d660216dd9f11fe6d082147ded7
rezatrimahardhika <rezatrimahardhika@gmail.com>
2020-09-08 01:33:35 +0700

    pippi mode

---
+++ index.js
@@ -0,0 +1,30 @@
+require('dotenv').config();
+const Discord = require('discord.js');
+const bot = new Discord.Client();
+const TOKEN = process.env.TOKEN;
+
+bot.login(TOKEN);
+
+function random_item(items) {
+  return items[Math.floor(Math.random()*items.length)];
+}
+
+bot.on('ready', () => {
+  console.info(`Logged in as ${bot.user.tag}!`);
+});
+
+bot.on('message', msg => {
+  if (msg.content === 'halo pippi' || msg.content === 'pippi' || msg.content === 'halo Pippi' || msg.content === 'Pippi') {
+    msg.reply('iya Pippi ? kamu mau nanya apa ? (Pippi cuma bisa jawab ya atau tidak)');
+  }else if(msg.content !== 'halo pippi' || msg.content !== 'halo Pippi'){
+    let n = msg.content.search("pippi");
+    let m = msg.content.search("Pippi");
+    if(n>-1 || m>-1){
+      console.log(msg.author.username)
+      if(msg.author.username!=="pippi" || msg.author.username!=="Pippi"){
+        let answer=["iya Pippi..","tidak Pippi.."]
+        msg.reply(random_item(answer));
+      }
+    }
+  }
+});
